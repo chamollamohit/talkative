27d456952f4f6f587666d374dcb59c114719d6f0
chamollamohit <mohitchamolla@gmail.com>
2026-01-27 16:38:22 +0530

    feat: initialize frontend with Vite and backend with express

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import  cookieparser from 'cookie-parser'
+import cors from 'cors'
+import { connectDB } from './lib/db.js'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 4000
+const app = express()
+
+
+
+app.use(express.json())
+app.use(cookieparser)
+app.use(express.urlencoded({extended: true}))
+app.use(
+    cors({
+        origin:['http://localhost:5173'],
+        credentials: true
+    })
+)
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on ${PORT}`);
+    connectDB()
+})
+
+
