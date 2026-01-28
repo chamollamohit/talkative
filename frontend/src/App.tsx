773b315d4517771eb423aea9398567e77b392197
chamollamohit <mohitchamolla@gmail.com>
2026-01-29 00:28:10 +0530

    feat: login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
+  const authUser = false;
   return (
-    <div className='text-5xl'>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={
+            authUser ? <ProfilePage /> : <Navigate to="/login" />
+          }
+        />
+      </Routes>
 
-export default App
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
