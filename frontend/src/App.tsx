270611049c85f895b41e6fa1393ec1d50c393c89
chamollamohit <mohitchamolla@gmail.com>
2026-01-29 17:38:35 +0530

    feat: sign-up & login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
@@ -6,36 +6,50 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/userAuthStore";
+import { Loader } from "lucide-react";
 
 const App = () => {
-  const authUser = false;
-  return (
-    <div>
-      <Routes>
-        <Route
-          path="/"
-          element={authUser ? <HomePage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route
-          path="/profile"
-          element={
-            authUser ? <ProfilePage /> : <Navigate to="/login" />
-          }
-        />
-      </Routes>
+    const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
-      <Toaster />
-    </div>
-  );
+    useEffect(() => {
+        checkAuth();
+    }, [checkAuth]);
+
+    if (isCheckingAuth && !authUser) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <Loader className="size-10 animate-spin" />
+            </div>
+        );
+    }
+    return (
+        <div>
+            <Routes>
+                <Route
+                    path="/"
+                    element={authUser ? <HomePage /> : <Navigate to="/login" />}
+                />
+                <Route
+                    path="/signup"
+                    element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+                />
+                <Route
+                    path="/login"
+                    element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+                />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route
+                    path="/profile"
+                    element={
+                        authUser ? <ProfilePage /> : <Navigate to="/login" />
+                    }
+                />
+            </Routes>
+
+            <Toaster />
+        </div>
+    );
 };
 
 export default App;
---
+++ frontend/src/store/userAuthStore.ts
@@ -0,0 +1,120 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+import { io, Socket } from "socket.io-client"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+
+
+interface authStoreState {
+    authUser: {
+        _id: string,
+        fullname: string,
+        email: string,
+        profilePic: string
+    } | null,
+    isSigningUp: boolean,
+    isLoggingIn: boolean,
+    isUpdatingProfile: boolean,
+    isCheckingAuth: boolean,
+    onlineUsers: string[],
+    socket: Socket | null 
+    connectSocket: () => void,
+    disconnectSocket: () => void,
+    checkAuth: () => void,
+    signup: (data: { userName: string, fullName: string, email: string, password: string }) => void,
+    login: (data: { email: string, password: string }) => void,
+    logout: () => void,
+    updateProfile: (data: {profilePic: string}) => void
+}
+
+export const useAuthStore = create<authStoreState>((set, get) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+    onlineUsers: [],
+    socket: null,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+
+            set({ authUser: res.data });
+            get().connectSocket()
+        } catch (error) {
+            console.log("Error in checkAuth:", error);
+            set({ authUser: null });
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
+    signup: async (data: { userName: string, fullName: string, email: string, password: string }) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({ authUser: res.data });
+            toast.success("Account created successfully");
+            get().connectSocket()
+        } catch (error) {
+            toast.error("Issue in Sign-Up");
+        } finally {
+            set({ isSigningUp: false });
+        }
+    },
+    login: async (data: { email: string, password: string }) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data });
+            toast.success("Logged in successfully");
+            get().connectSocket()
+
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    },
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+            get().disconnectSocket();
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    },
+    updateProfile: async (data: { profilePic: string }) => {
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({ authUser: res.data });
+            toast.success("Profile updated successfully");
+        } catch (error) {
+            console.log("error in update profile:", error);
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isUpdatingProfile: false });
+        }
+    },
+    connectSocket: () => {
+        const { authUser } = get();
+        if (!authUser || get().socket?.connected) return;
+        const socket = io(BASE_URL, {
+            query: {
+                userId: authUser._id
+            }
+        }); 
+        socket.connect();
+        set({ socket: socket });
+
+        socket.on("getOnlineUsers", (userIds) => {
+            set({ onlineUsers: userIds })
+        })
+    },
+    disconnectSocket: () => {
+        if (get().socket?.connected) get().socket?.disconnect();
+    }
+})) 
